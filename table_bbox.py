d8eeb0e7764fea5666c43050fca0b06cfe35772d
Sree Sankar S <100954808+sreesankar711@users.noreply.github.com>
2023-07-02 13:55:13 +0530

    Update table_bbox.py

--- table_bbox.py
+++ table_bbox.py
@@ -2,7 +2,6 @@ import math
 import os
 import cv2
 import sys
-import argparse
 from pathlib import Path
 from typing import Iterable
 import numpy as np
@@ -30,58 +29,7 @@ def rescale_bboxes(out_bbox, size):
     return b
 
 
-def get_args_parser():
-    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
-    parser.add_argument('--lr', default=1e-4, type=float)
-    parser.add_argument('--lr_backbone', default=1e-5, type=float)
-    parser.add_argument('--batch_size', default=6, type=int)
-    parser.add_argument('--weight_decay', default=1e-4, type=float)
-    parser.add_argument('--epochs', default=300, type=int)
-    parser.add_argument('--lr_drop', default=200, type=int)
-    parser.add_argument('--clip_max_norm', default=0.1, type=float, help='gradient clipping max norm')
-    # Model parameters
-    parser.add_argument('--frozen_weights', type=str, default=None, help="Path to the pretrained model. If set, only the mask head will be trained")
-    # * Backbone
-    parser.add_argument('--backbone', default='resnet50', type=str, help="Name of the convolutional backbone to use")
-    parser.add_argument('--dilation', action='store_true', help="If true, we replace stride with dilation in the last convolutional block (DC5)")
-    parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'), help="Type of positional embedding to use on top of the image features")
-    # * Transformer
-    parser.add_argument('--enc_layers', default=6, type=int, help="Number of encoding layers in the transformer")
-    parser.add_argument('--dec_layers', default=6, type=int, help="Number of decoding layers in the transformer")
-    parser.add_argument('--dim_feedforward', default=2048, type=int, help="Intermediate size of the feedforward layers in the transformer blocks")
-    parser.add_argument('--hidden_dim', default=256, type=int, help="Size of the embeddings (dimension of the transformer)")
-    parser.add_argument('--dropout', default=0.1, type=float, help="Dropout applied in the transformer")
-    parser.add_argument('--nheads', default=8, type=int, help="Number of attention heads inside the transformer's attentions")
-    parser.add_argument('--num_queries', default=10, type=int, help="Number of query slots")
-    parser.add_argument('--pre_norm', action='store_true')
-    # * Segmentation
-    parser.add_argument('--masks', action='store_true', help="Train segmentation head if the flag is provided")
-    # # Loss
-    parser.add_argument('--no_aux_loss', dest='aux_loss', action='store_false', help="Disables auxiliary decoding losses (loss at each layer)")
-    # * Matcher
-    parser.add_argument('--set_cost_class', default=1, type=float, help="Class coefficient in the matching cost")
-    parser.add_argument('--set_cost_bbox', default=5, type=float, help="L1 box coefficient in the matching cost")
-    parser.add_argument('--set_cost_giou', default=2, type=float, help="giou box coefficient in the matching cost")
-    # * Loss coefficients
-    parser.add_argument('--mask_loss_coef', default=1, type=float)
-    parser.add_argument('--dice_loss_coef', default=1, type=float)
-    parser.add_argument('--bbox_loss_coef', default=5, type=float)
-    parser.add_argument('--giou_loss_coef', default=2, type=float)
-    parser.add_argument('--eos_coef', default=0.1, type=float, help="Relative classification weight of the no-object class")
-    # dataset parameters
-    parser.add_argument('--dataset_file', default='face')
-    parser.add_argument('--data_path', type=str)
-    parser.add_argument('--data_panoptic_path', type=str)
-    parser.add_argument('--remove_difficult', action='store_true')
-    parser.add_argument('--output_dir', default='', help='path where to save the results, empty for no saving')
-    parser.add_argument('--device', default='cuda', help='device to use for training / testing')
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--thresh', default=0.5, type=float)
-    return parser
-
-
-@torch.no_grad()
-def infer(img_sample, model, postprocessors, device, output_path):
+def infer(img_sample, model, postprocessors, device, output_path, args):
     model.eval()
     filename = os.path.basename(img_sample)
     orig_image = Image.open(img_sample)
@@ -145,9 +93,8 @@ def infer(img_sample, model, postprocessors, device, output_path):
     return bounding_boxes
 
 
-def table_evaluate(img_path, checkpoint ):
-    parser = argparse.ArgumentParser('DETR training and evaluation script')
-    args = parser.parse_args()
+def table_evaluate(img_path, checkpoint):
+    args = get_default_args()
     args.resume = checkpoint
 
     if args.output_dir:
@@ -156,11 +103,22 @@ def table_evaluate(img_path, checkpoint ):
     device = torch.device(args.device)
 
     model, _, postprocessors = build_model(args)
-  
+
     if args.resume:
         checkpoint = torch.load(args.resume, map_location='cpu')
         model.load_state_dict(checkpoint['model'])
     model.to(device)
 
+    return infer(img_path, model, postprocessors, device, args.output_dir, args)
 
-    return infer(img_path, model, postprocessors, device, args.output_dir)
+
+def get_default_args():
+    class Args:
+        def __init__(self):
+            self.resume = ''
+            self.output_dir = ''
+            self.device = 'cuda'
+            self.thresh = 0.5
+
+    args = Args()
+    return args
